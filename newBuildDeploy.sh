8213f84428864ebd1094458616f1b33060c964f3
Lamar Daughma <lite5h4dow@gmail.com>
2021-08-03 09:15:24 +0100

    updating stick example

--- newBuildDeploy.sh
+++ newBuildDeploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose up && scp ./target/arm-unknown-linux-gnueabihf/release/camera_rig pi@192.168.0.25:/home/pi/camera-rig
-sudo rm -rf ./target
+docker compose up && scp ./target/arm-unknown-linux-gnueabihf/release/camera_rig pi@192.168.1.10:/home/pi/camera-rig
+rm -rf ./target
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,70 @@
-extern crate sysfs_gpio;
+use pasts::Loop;
+use std::task::Poll::{self, Pending, Ready};
+use stick::{Controller, Event, Listener};
 
-use sysfs_gpio::{Direction, Pin};
-use std::thread::{sleep, spawn};
-use std::time::Duration;
+type Exit = usize;
 
-use pasts::prelude::*;
-
-use stick::{Controller, Event};
-
-fn micros_pulse(pin: Pin, duration: u64){
-    pin.set_value(1).unwrap();
-    sleep(Duration::from_micros(duration));
-    pin.set_value(0).unwrap();
+struct State {
+    listener: Listener,
+    controllers: Vec<Controller>,
+    rumble: (f32, f32),
 }
 
-async fn controller_thread(){
-    let mut listener = Controller::listener();
-    let mut ctlrs = Vec::<Controller>::new();
+impl State {
+    fn connect(&mut self, controller: Controller) -> Poll<Exit> {
+        println!(
+            "Connected p{}, id: {:016X}, name: {}",
+            self.controllers.len() + 1,
+            controller.id(),
+            controller.name(),
+        );
+        self.controllers.push(controller);
+        Pending
+    }
 
-    println!("Hello from the controller thread");
-
-    'e: loop{
-        match poll![listener, poll!(ctlrs)].await.1{
-            (_, Event::Connect(new)) =>{
-                println!(
-                    "Connected p{}, id: {:04X}_{:04X}_{:04X}_{:04X}, name: {}",
-                    ctlrs.len()+1,
-                    new.id()[0],
-                    new.id()[1],
-                    new.id()[2],
-                    new.id()[3],
-                    new.name(),
-                );
-                ctlrs.push(*new);
+    fn event(&mut self, id: usize, event: Event) -> Poll<Exit> {
+        let player = id + 1;
+        println!("p{}: {}", player, event);
+        match event {
+            Event::Disconnect => {
+                self.controllers.swap_remove(id);
             }
-
-            (id, Event::Disconnect) => {
-                println!("Disconnected p{}",id + 1);
-                ctlrs.swap_remove(id);
+            Event::MenuR(true) => return Ready(player),
+            Event::ActionA(pressed) => {
+                self.controllers[id].rumble(f32::from(u8::from(pressed)));
             }
-
-            (id, Event::Home(true)) => {
-                println!("p{} ended the session", id+1);
-                break 'e;
+            Event::ActionB(pressed) => {
+                self.controllers[id].rumble(0.5 * f32::from(u8::from(pressed)));
             }
-
-            (id, event)=>{
-                println!("p{}: {}", id+1, event);
-                match event{
-                    Event::ActionA(pressed) => {
-                        ctlrs[id].rumble(if pressed {1.0} else {0.0});
-                    }
-                    Event::ActionB(pressed) => {
-                        ctlrs[id].rumble(if pressed {0.3} else {0.0});
-                    }
-                    _ => {}
-                }
+            Event::BumperL(pressed) => {
+                self.rumble.0 = f32::from(u8::from(pressed));
+                self.controllers[id].rumble(self.rumble);
             }
+            Event::BumperR(pressed) => {
+                self.rumble.1 = f32::from(u8::from(pressed));
+                self.controllers[id].rumble(self.rumble);
+            }
+            _ => {}
         }
+        Pending
     }
 }
 
-fn main(){
-    spawn(|| {exec!(controller_thread())});
-    let step = Pin::new(23);
-    step.with_exported(||{
-        step.set_direction(Direction::Out).unwrap();
-        loop{
-            micros_pulse(step, 1);
-            sleep(Duration::from_micros(650));
-        }
-    }).unwrap()
+async fn event_loop() {
+    let mut state = State {
+        listener: Listener::default(),
+        controllers: Vec::new(),
+        rumble: (0.0, 0.0),
+    };
 
+    let player_id = Loop::new(&mut state)
+        .when(|s| &mut s.listener, State::connect)
+        .poll(|s| &mut s.controllers, State::event)
+        .await;
+
+    println!("p{} ended the session", player_id);
+}
+
+fn main() {
+    pasts::block_on(event_loop());
 }
